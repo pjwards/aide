21726d92c971965af264d3a4cbb25c11de6a7114
Jisung <cbajs20@gmail.com>
2016-04-11 13:14:25 +0900

    Init aide project about Sponsor and Assets
    Init aide project about Sponsor and Assets
    
    Issue :

---
+++ src/main/java/com/pjwards/aide/service/Assets/AssetsService.java
@@ -0,0 +1,19 @@
+package com.pjwards.aide.service.Assets;
+
+
+import com.pjwards.aide.domain.Assets;
+import com.pjwards.aide.exception.AssetsNotFoundException;
+
+import java.util.List;
+
+public interface AssetsService {
+    public List<Assets> findAll();
+
+    public Assets add(Assets added);
+
+    public Assets findById(Long id) throws AssetsNotFoundException;
+
+    public Assets update(Assets updated) throws AssetsNotFoundException;
+
+    public Assets deleteById(Long id) throws AssetsNotFoundException;
+}
